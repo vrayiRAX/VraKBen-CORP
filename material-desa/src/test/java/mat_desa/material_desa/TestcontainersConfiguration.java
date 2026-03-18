8d83b1f8f6b906e4d6f38545f82c7906c91290c0
vrayiRAX <ia.badilla@duocuc.cl>
2026-03-18 13:58:18 -0300

    primer commit

---
+++ material-desa/src/test/java/mat_desa/material_desa/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package mat_desa.material_desa;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}

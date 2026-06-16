dc83cd27704522bad43d2bd099770ba44921a279
ZorVaK749 <ZorVaK.749@gmail.com>
2026-06-16 11:16:59 -0400

    fix(order-management): importar PutMapping faltante en StockClient

--- ms-order-management/src/main/java/ms_order_management/order_management/client/StockClient.java
+++ ms-order-management/src/main/java/ms_order_management/order_management/client/StockClient.java
@@ -3,6 +3,7 @@ package ms_order_management.order_management.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "catalog")
